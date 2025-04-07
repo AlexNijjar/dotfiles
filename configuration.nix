32c048b69d7e5ad3960e7c6bdcf43d9fa8d4fa14
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-06 23:41:24 -0700

    Fix syntax error

--- configuration.nix
+++ configuration.nix
@@ -28,12 +28,13 @@
 
   users = {
     users.alex = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    shell = pkgs.nushell;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIdZWguoU6C7AIsM4+DDVx5RnjCdZ6xY0yhgkMZyAOBT alexander.nijjar@icloud.com"
-    ];
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      shell = pkgs.nushell;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIdZWguoU6C7AIsM4+DDVx5RnjCdZ6xY0yhgkMZyAOBT alexander.nijjar@icloud.com"
+      ];
+    };
     users.root.shell = pkgs.nushell;
     defaultUserShell = pkgs.nushell;
   };

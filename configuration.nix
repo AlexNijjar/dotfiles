02ba11e6e77765afe8d432802fb802f1d197db77
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-06 23:39:38 -0700

    Fix syntax error

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  users {
+  users = {
     users.alex = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];

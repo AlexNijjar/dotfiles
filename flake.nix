69d78cdc54484593555abe1bcb3e5a5a13352c5d
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-06 21:33:29 -0700

    Fix missing semicolon

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
-  }
+  };
   
   outputs = { self, nixpkgs, sops-nix }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {

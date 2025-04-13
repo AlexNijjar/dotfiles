71aeec4860227576c89259c8c73f2c5ad1827e85
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-13 00:58:44 -0700

    Add more keybinds

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -29,10 +29,25 @@
 
       "$mod" = "SUPER";
       bind = [
+        "$mod, Q, killactive"
+        "$mod, DELETE, exit"
+        "$mod, V, togglefloating"
+        "$mod, F, fullscreen"
+        "$mod, P, pin"
+
         "$mod, F1, exec, walker"
         "$mod, F2, exec, ghostty"
         "$mod, F3, exec, librewolf"
         "$mod, F4, exec, thunar"
+
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        "XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
+        "xf86audioraisevolume, exec, swayosd-client --output-volume raise"
+        "xf86audiolowervolume, exec, swayosd-client --output-volume lower"
       ] ++ (
         builtins.concatLists (builtins.genList (i:
           let ws = i + 1;
@@ -43,6 +58,25 @@
         )
         9)
       );
+
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", mouse:275, workspace, +1"
+        ", mouse:276, workspace, -1"
+      ];
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
     };
   };
 }

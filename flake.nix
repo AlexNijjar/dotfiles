79d283c99aa1d72c7ce20ab0ea6118628bb6a972
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-12 23:19:35 -0700

    Add missing ;

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
     catppuccin.url = "github:catppuccin/nix";
     sops-nix.url = "github:Mic92/sops-nix";
-    walker.url = "github:abenz1267/walker"
+    walker.url = "github:abenz1267/walker";
   };
   
   outputs = { self, nixpkgs, disko, home-manager, sops-nix, catppuccin }: {

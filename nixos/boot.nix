c081a14d293e062ef0da01b95f2cae4d1f1683c9
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-12 19:01:49 -0700

    Move parts out of hardware config

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -6,5 +6,13 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+      kernelModules = [ ];
+    };
   };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,5 +12,7 @@
     };
   };
 
+  hardware.cpu.amd.updateMicrocode = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "24.11";
 }

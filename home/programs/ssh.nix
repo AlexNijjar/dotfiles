4d4b545daeef4ea1f2502ee4d66d7dbcde2be12d
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-13 01:56:56 -0700

    Use hostname

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -4,22 +4,22 @@
     addKeysToAgent = "yes";
     matchBlocks = {
       github = {
-        host = "github.com";
+        hostname = "github.com";
         identityFile = "~/.ssh/alex-arch";
         user = "git";
       };
       hf = {
-        host = "hf.co";
+        hostname = "hf.co";
         identityFile = "~/.ssh/alex-arch";
         user = "git";
       };
       tinybox = {
-        host = "10.0.4.17";
+        hostname = "10.0.4.17";
         identityFile = "~/.ssh/alex-arch";
         user = "alex";
       };
       alex = {
-        host = "10.0.4.2";
+        hostname = "10.0.4.2";
         identityFile = "~/.ssh/alex-arch";
         user = "alex";
       };

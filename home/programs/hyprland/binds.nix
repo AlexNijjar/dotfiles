17ca257088c2f7d29ee20418c331e3a2dae6ab1c
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-14 02:41:13 -0700

    Fix audio binds

--- home/programs/hyprland/binds.nix
+++ home/programs/hyprland/binds.nix
@@ -41,10 +41,5 @@
       "$mod, mouse:272, movewindow"
       "$mod, mouse:273, resizewindow"
     ];
-
-    bindel = [
-      ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-    ];
   };
 }

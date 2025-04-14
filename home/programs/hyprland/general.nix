5358d29687f71ee7b7def879067e64e454a3befc
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-13 17:01:49 -0700

    Fix border color

--- home/programs/hyprland/general.nix
+++ home/programs/hyprland/general.nix
@@ -4,8 +4,8 @@
     gaps_out = 5;
     border_size = 2;
 
-    col.active_border = "$mauve $sapphire 45deg";
-    col.inactive_border = "$base";
+    "col.active_border" = "$mauve $sapphire 45deg";
+    "col.inactive_border" = "$base";
 
     resize_on_border = true;
     allow_tearing = true;

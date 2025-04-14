f5316a1c3473d5a61c9b240a86bdbc04c0259620
AlexNijjar <alexander.nijjar@icloud.com>
2025-04-13 17:29:41 -0700

    Configure ghostty

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -1,5 +1,18 @@
 {
   programs.ghostty = {
     enable = true;
+
+    settings = {
+      theme = "catppuccin-mocha";
+      font-size = 13;
+      background-opacity = 0.9;
+      keybind = [
+        "performable:ctrl+c=copy_to_clipboard"
+        "ctrl+v=paste_from_clipboard"
+        "ctrl+a=select_all"
+        "ctrl+k=clear_screen"
+        "ctrl+l=clear_screen"
+      ];
+    };
   };
 }
